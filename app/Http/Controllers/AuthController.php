b81aa159ce5c6e6fada569725155999aa6655ac2
Youssef Ben Attar <youssefdisvitas@gmail.com>
2024-03-18 14:19:27 +0000

    register , login , update , logout

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $attrs = $request->validate([
+            "name" => "required|string",
+            "email" => "required|email|unique:users,email",
+            "password" => "required|min:5|confirmed"
+        ]);
+        $user = User::create([
+            "name" => $attrs['name'],
+            "email" => $attrs['email'],
+            "password" => bcrypt($attrs["password"])
+        ]);
+        return response([
+            "user" => $user,
+            "token" => $user->createToken('secret')->plainTextToken
+        ], 200);
+    }
+
+    public function login(Request $request)
+    {
+        $attrs = $request->validate([
+            "email" => "required|email",
+            "password" => "required|min:5"
+        ]);
+        if (!Auth::attempt($attrs)) {
+            return response([
+                "message" => "invalid credentials"
+            ], 403);
+        }
+        return response([
+            'user' => auth()->user(),
+            'token' => auth()->user()->createToken('secret')->plainTextToken
+        ], 200);
+    }
+
+    public  function update (Request $request){
+        $attrs = $request->validate(["name" => "required|string"]);
+        auth()->user()->update(["name" => $attrs["name"]]);
+        return response([
+            "message" => "user updated",
+            "user" => auth()->user()
+        ],200);
+    }
+
+    public function logout(Request $request){
+        auth()->user()->tokens()->delete();
+        return response ([
+            "message" => "logout success"
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get("/test",function (){return response (['message' => "Api Is Working"]);});
+Route::post("/register" , [AuthController::class,"register"]);
+Route::post("/login" , [AuthController::class,"login"]);
+Route::group(['middleware'=>['auth:sanctum']],function (){
+    Route::put('/put', [AuthController::class,'update']);
+    Route::post('/logout', [AuthController::class,'logout']);
 });
